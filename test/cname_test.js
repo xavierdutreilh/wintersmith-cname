49ba9ceff6726cbc71af8a0d68a6811eed4c1a3c
Xavier Dutreilh <xavier@dutreilh.com>
2016-06-07 21:37:45 +0200

    Refactor test

--- test/cname_test.js
+++ test/cname_test.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 function read(filename) {
-  return fs.readFileSync(filename, {'encoding': 'utf8'});
+  return fs.readFileSync(filename, 'utf8');
 }
 
 exports.cname = {
   'build': (test) => {
-    const actual = read('tmp/CNAME');
-    const expected = read('test/expected/CNAME');
+    const actual = read(path.join(__dirname, '..', 'tmp', 'CNAME'));
+    const expected = read(path.join(__dirname, 'expected', 'CNAME'));
 
     test.equal(actual, expected, 'should build the CNAME file');
 
