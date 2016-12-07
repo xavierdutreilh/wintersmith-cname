b52bd47cfa946a1e920cfd1adacd3b7807e78ef4
Xavier Dutreilh <xavier@dutreilh.com>
2016-12-07 16:59:38 +0100

    Stop using path module

--- test/cname_test.js
+++ test/cname_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 function read(filename) {
   return fs.readFileSync(filename, 'utf8');
@@ -9,8 +8,8 @@ function read(filename) {
 
 exports.cname = {
   'build': (test) => {
-    const actual = read(path.join(__dirname, '..', 'tmp', 'CNAME'));
-    const expected = read(path.join(__dirname, 'expected', 'CNAME'));
+    const actual = read(`${__dirname}/../tmp/CNAME`);
+    const expected = read(`${__dirname}/expected/CNAME`);
 
     test.equal(actual, expected, 'should build the CNAME file');
 
